5f80b248b1548cf9ec49303f747c13bae8b2ba2a
zsnahid <zahidsadmannahid@gmail.com>
2024-09-19 17:30:20 +0600

    login page complete

---
+++ js/login.js
@@ -0,0 +1,14 @@
+console.clear();
+
+document.getElementById('login-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    const phoneNum = document.getElementById('phone-number').value;
+    const pin = document.getElementById('pin').value;
+    console.log(phoneNum, pin);
+    if (phoneNum === '5' && pin === '1234') {
+        console.log('Login successful');
+    }
+    else {
+        alert('Wrong phone number or pin');
+    }
+})
